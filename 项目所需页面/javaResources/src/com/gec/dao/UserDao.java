8cdc2c977ca58f5bda3f0fac4d268e2fc9730368
chen123 <1501862055@qq.com>
2021-12-05 20:02:32 +0800

    commit message

--- 项目所需页面/javaResources/src/com/gec/dao/UserDao.java
+++ 项目所需页面/javaResources/src/com/gec/dao/UserDao.java
@@ -7,8 +7,11 @@ import org.apache.commons.dbutils.handlers.BeanHandler;
 
 public class UserDao {
     public User findUserByUsernameAndPassword(String username, String password) throws Exception {
+
         QueryRunner queryRunner = new QueryRunner(C3P0Utils.getDataSource());
+
         String[] args = {username, password};
+
         User user = queryRunner.query("select * from user where username=? and password=?", args, new BeanHandler<>(User.class));
         return user;
     }
--- 项目所需页面/javaResources/src/com/gec/servlet/LoginServlet.java
+++ 项目所需页面/javaResources/src/com/gec/servlet/LoginServlet.java
@@ -15,16 +15,20 @@ public class LoginServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //获取用户账号密码
         String username = req.getParameter("username");
         String password = req.getParameter("password");
+        //对账号密码进行判断
         User user = userService.login(username,password);
 
         if (user!=null)
         {
+            //正确就是登录成功 查询结果不为空 就是账号密码正确
             HttpSession session = req.getSession();
             session.setAttribute("user",user);
             resp.sendRedirect("index.jsp");
         }else {
+            //错误登录失败
             req.setAttribute("error","no");
             req.getRequestDispatcher("login.jsp").forward(req,resp);
         }
