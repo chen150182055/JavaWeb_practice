f62ddc5f2821e07724b2798b31ad982f9c7ab364
chen123 <1501862055@qq.com>
2021-12-05 09:52:37 +0800

    commit message

--- 项目所需页面/out/artifacts/_Web_exploded/javaResources/src/com/gec/servlet/LoginServlet.java
+++ 项目所需页面/out/artifacts/_Web_exploded/javaResources/src/com/gec/servlet/LoginServlet.java
@@ -3,7 +3,6 @@ package com.gec.servlet;
 import com.gec.entity.User;
 import com.gec.service.UserService;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
